9c25c58b137e83bd12301256cd1d4dee9cc06768
Vadim Eisenberg <vadime@il.ibm.com>
2021-09-11 18:35:17 +0300

    add actual sending of managed clusters

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-logr/logr v0.2.1
 	github.com/go-logr/zapr v0.2.0
 	github.com/jackc/pgx/v4 v4.11.0
+	github.com/open-cluster-management/api v0.0.0-20210527013639-a6845f2ebcb1
 	github.com/openshift/api v3.9.0+incompatible
 	go.uber.org/zap v1.19.0
 	k8s.io/client-go v0.20.5
--- pkg/managedclusters/managedclusters.go
+++ pkg/managedclusters/managedclusters.go
@@ -4,11 +4,13 @@
 package managedclusters
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v4/pgxpool"
+	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
 	"github.com/open-cluster-management/hub-of-hubs-nonk8s-api/pkg/authentication"
 )
 
@@ -32,6 +34,26 @@ func ManagedClusters(dbConnectionPool *pgxpool.Pool) gin.HandlerFunc {
 		fmt.Fprintf(gin.DefaultWriter, "got authenticated user: %v\n", user)
 		fmt.Fprintf(gin.DefaultWriter, "user groups: %v\n", groups)
 
-		c.String(http.StatusOK, fmt.Sprintf("Managed Clusters for user %s, groups = %v\n", user, groups))
+		rows, err := dbConnectionPool.Query(context.TODO(), `SELECT payload FROM status.managed_clusters`)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "internal error")
+			fmt.Fprintf(gin.DefaultWriter, "error in quering managed clusters: %v\n", err)
+		}
+
+		var managedClusters []*clusterv1.ManagedCluster
+
+		for rows.Next() {
+			managedCluster := &clusterv1.ManagedCluster{}
+
+			err := rows.Scan(managedCluster)
+			if err != nil {
+				fmt.Fprintf(gin.DefaultWriter, "error in scanning a managed cluster: %v\n", err)
+				continue
+			}
+
+			managedClusters = append(managedClusters, managedCluster)
+		}
+
+		c.JSON(http.StatusOK, managedClusters)
 	}
 }
