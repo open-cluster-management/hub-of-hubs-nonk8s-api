b42712246fc717a2448a9fea0ba8ea63357ba5a4
Vadim Eisenberg <vadime@il.ibm.com>
2021-09-12 22:29:10 +0300

    implement watch

--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 	github.com/open-cluster-management/api v0.0.0-20210527013639-a6845f2ebcb1
 	github.com/openshift/api v3.9.0+incompatible
 	go.uber.org/zap v1.19.0
+	k8s.io/apimachinery v0.20.5
 	k8s.io/client-go v0.20.5
 )
--- pkg/managedclusters/managedclusters.go
+++ pkg/managedclusters/managedclusters.go
@@ -5,14 +5,21 @@ package managedclusters
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v4/pgxpool"
 	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
 	"github.com/open-cluster-management/hub-of-hubs-nonk8s-api/pkg/authentication"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
+
+const syncIntervalInSeconds = 4
 
 // ManagedClusters middleware.
 func ManagedClusters(dbConnectionPool *pgxpool.Pool) gin.HandlerFunc {
@@ -34,26 +41,110 @@ func ManagedClusters(dbConnectionPool *pgxpool.Pool) gin.HandlerFunc {
 		fmt.Fprintf(gin.DefaultWriter, "got authenticated user: %v\n", user)
 		fmt.Fprintf(gin.DefaultWriter, "user groups: %v\n", groups)
 
-		rows, err := dbConnectionPool.Query(context.TODO(), `SELECT payload FROM status.managed_clusters`)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "internal error")
-			fmt.Fprintf(gin.DefaultWriter, "error in quering managed clusters: %v\n", err)
+		query := sqlQuery(user, groups)
+
+		if _, watch := c.GetQuery("watch"); watch {
+			handleRowsForWatch(c, query, dbConnectionPool)
+			return
 		}
 
-		var managedClusters []*clusterv1.ManagedCluster
+		handleRows(c, query, dbConnectionPool)
+	}
+}
 
-		for rows.Next() {
-			managedCluster := &clusterv1.ManagedCluster{}
+func sqlQuery(user string, groups []string) string {
+	// to be used in authorization
+	_ = user
+	_ = groups
 
-			err := rows.Scan(managedCluster)
-			if err != nil {
-				fmt.Fprintf(gin.DefaultWriter, "error in scanning a managed cluster: %v\n", err)
-				continue
+	return "SELECT payload FROM status.managed_clusters"
+}
+
+func handleRowsForWatch(ginCtx *gin.Context, query string, dbConnectionPool *pgxpool.Pool) {
+	w := ginCtx.Writer
+	header := w.Header()
+	header.Set("Transfer-Encoding", "chunked")
+	header.Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	ticker := time.NewTicker(syncIntervalInSeconds * time.Second)
+
+	ctx, cancelContext := context.WithCancel(context.Background())
+	defer cancelContext()
+
+	for {
+		select {
+		case <-w.CloseNotify():
+			ticker.Stop()
+			cancelContext()
+
+			return
+		case <-ticker.C:
+			if ginCtx.Err() != nil || ginCtx.IsAborted() {
+				ticker.Stop()
+				cancelContext()
+
+				return
 			}
 
-			managedClusters = append(managedClusters, managedCluster)
+			doHandleRowsForWatch(ctx, w, query, dbConnectionPool)
+		}
+	}
+}
+
+func doHandleRowsForWatch(ctx context.Context, w io.Writer, query string, dbConnectionPool *pgxpool.Pool) {
+	rows, err := dbConnectionPool.Query(ctx, query)
+	if err != nil {
+		fmt.Fprintf(gin.DefaultWriter, "error in quering managed clusters: %v\n", err)
+	}
+
+	for rows.Next() {
+		managedCluster := &clusterv1.ManagedCluster{}
+
+		err := rows.Scan(managedCluster)
+		if err != nil {
+			continue
 		}
 
-		c.JSON(http.StatusOK, managedClusters)
+		watchEvent := &metav1.WatchEvent{Type: "ADDED", Object: runtime.RawExtension{Object: managedCluster}}
+
+		json, err := json.Marshal(watchEvent)
+		if err != nil {
+			fmt.Fprintf(gin.DefaultWriter, "error in json marshalling: %v\n", err)
+			continue
+		}
+
+		_, err = w.Write(json)
+
+		if err != nil {
+			fmt.Fprintf(gin.DefaultWriter, "error in writing response: %v\n", err)
+			continue
+		}
+
+		w.(http.Flusher).Flush()
 	}
+}
+
+func handleRows(c *gin.Context, query string, dbConnectionPool *pgxpool.Pool) {
+	rows, err := dbConnectionPool.Query(context.TODO(), query)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "internal error")
+		fmt.Fprintf(gin.DefaultWriter, "error in quering managed clusters: %v\n", err)
+	}
+
+	var managedClusters []*clusterv1.ManagedCluster
+
+	for rows.Next() {
+		managedCluster := &clusterv1.ManagedCluster{}
+
+		err := rows.Scan(managedCluster)
+		if err != nil {
+			fmt.Fprintf(gin.DefaultWriter, "error in scanning a managed cluster: %v\n", err)
+			continue
+		}
+
+		managedClusters = append(managedClusters, managedCluster)
+	}
+
+	c.JSON(http.StatusOK, managedClusters)
 }
