bc1ba130e8bd8153ed72385f1d38e78ac11b3e8e
Vadim Eisenberg <vadime@il.ibm.com>
2021-09-21 19:01:09 +0300

    intermediate commit

--- pkg/managedclusters/authorization.go
+++ pkg/managedclusters/authorization.go
@@ -12,28 +12,135 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	opatypes "github.com/open-policy-agent/opa/server/types"
 )
 
 var errStatusNotOK = errors.New("response status not HTTP OK")
 
+const (
+	sqlFalse     = "FALSE"
+	sqlTrue      = "TRUE"
+	denyAll      = "WHERE " + sqlFalse
+	termsTypeRef = "ref"
+)
+
 func filterByAuthorization(user string, groups []string, authorizationURL string, logWriter io.Writer) string {
 	compileResponse, err := getPartialEvaluation(user, groups, authorizationURL)
 	if err != nil {
 		fmt.Fprintf(logWriter, "unable to get partial evaluation response %v\n", err)
-		return "WHERE FALSE"
+		return denyAll
 	}
 
 	resultMap, ok := (*compileResponse.Result).(map[string]interface{})
 	if !ok {
 		fmt.Fprintf(logWriter, "unable to convert result to map\n")
-		return "WHERE FALSE"
+		return denyAll
 	}
 
 	fmt.Fprintf(logWriter, "got partial evaluation response result %v\n", resultMap)
 
-	return "WHERE TRUE"
+	queries, ok := resultMap["queries"].([]interface{})
+	if !ok || len(queries) < 1 {
+		return denyAll
+	}
+
+	var sb strings.Builder
+
+	sb.WriteString("WHERE ")
+
+	for _, rawQuery := range queries {
+		query, ok := rawQuery.([]interface{})
+		if !ok {
+			fmt.Fprintf(logWriter, "unable to convert query to an array: %v\n", rawQuery)
+			continue
+		}
+
+		if len(query) < 1 {
+			continue
+		}
+
+		fmt.Fprintf(logWriter, "handle query: %v\n", query)
+
+		sb.WriteString("(")
+		for _, rawExpression := range query {
+			expression, ok := rawExpression.(map[string]interface{})
+			if !ok {
+				fmt.Fprintf(logWriter, "unable to convert expression to a map: %v\n", rawExpression)
+				sb.WriteString(sqlFalse + ") ")
+				continue
+			}
+
+			fmt.Fprintf(logWriter, "handle expression: %v\n", expression)
+
+			negated := false
+
+			rawNegated, ok := expression["negated"]
+			if ok {
+				convertedNegated, ok := rawNegated.(bool)
+				if ok {
+					negated = convertedNegated
+				}
+			}
+
+			rawTerms, ok := expression["terms"]
+			if !ok {
+				fmt.Fprintf(logWriter, "unable to get terms from expression: %v\n", expression)
+				sb.WriteString(sqlFalse + ") ")
+				continue
+			}
+
+			terms, ok := rawTerms.([]interface{})
+			if !ok {
+				fmt.Fprintf(logWriter, "unable to get terms array from expression: %v\n", expression)
+				sb.WriteString(sqlFalse + ") ")
+				continue
+			}
+
+			sb.WriteString("(")
+
+			handleTermsArray(terms, negated, &sb, logWriter)
+
+			sb.WriteString(") AND ")
+		}
+		sb.WriteString(sqlTrue) // TRUE to handle the last AND
+		sb.WriteString(") OR ")
+	}
+
+	sb.WriteString(sqlFalse) // for the last OR
+
+	returnString := sb.String()
+	fmt.Fprintf(logWriter, "using SQL: %s\n", returnString)
+
+	return returnString
+}
+
+func handleTermsArray(terms []interface{}, negated bool, sb *strings.Builder, logWriter io.Writer) {
+	fmt.Fprintf(logWriter, "handle terms as an array (negated = %t): %v\n", negated, terms)
+
+	if negated {
+		sb.WriteString("NOT (")
+	}
+
+	expression, err := getSQLExpression(terms)
+	if err == nil {
+		sb.WriteString(expression)
+	} else {
+		fmt.Fprintf(logWriter, "unable to get SQL expression: %v\n", err)
+		if negated {
+			sb.WriteString(sqlTrue)
+		} else {
+			sb.WriteString(sqlFalse)
+		}
+	}
+	if negated {
+		sb.WriteString(")")
+	}
+}
+
+func getSQLExpression(terms []interface{}) (string, error) {
+	return "", errors.New("not implemented")
 }
 
 func getPartialEvaluation(user string, groups []string, authorizationURL string) (*opatypes.CompileResponseV1, error) {
